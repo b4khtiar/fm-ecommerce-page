de7d6940ccb6dab73cf47c3d7bccca6e69162209
Bakhtiar <muhammadybakhtiar@gmail.com>
2024-06-25 04:54:56 +0700

    Add lightbox component

---
+++ src/components/Lightbox.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="lightbox">
+        <div class="wrapper">
+            <div class="close__button">
+                <svg @click="emit('closeLightbox')" class="close-icon" width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                        fill="currentColor" fill-rule="evenodd" />
+                </svg>
+            </div>
+            <ProductGallery :images="images" :thumbnails="thumbnails" :lightbox="true"/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ProductGallery from '@/components/ProductGallery.vue'
+const props = defineProps({
+    images: Array,
+    thumbnails: Array
+})
+const emit = defineEmits(['closeLightbox'])
+</script>
+
+<style scoped>
+.lightbox {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: hsla(0, 0%, 0%, 0.75);
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    backdrop-filter: blur(4px);
+}
+
+.wrapper {
+    position: relative;
+    max-width: 70vh;
+}
+
+.close__button {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 0.5rem;
+}
+
+.close-icon {
+    cursor: pointer;
+    color: var(--White);
+}
+.close-icon:hover {
+    color: var(--Orange);
+}
+</style>
--- src/components/ProductGallery.vue
+++ src/components/ProductGallery.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="product-gallery">
         <div class="slider">
-            <img :src="images[imgIndex]" alt="">
-            <div class="slider-control">
+            <img @click="showLightbox" :src="images[imgIndex]" alt="">
+            <div class="slider-control" :class="[lightbox ? 'lightbox-mode' : '']">
                 <div @click="previous" class="control-circle previous">
                     <img src="/images/icon-previous.svg" alt="">
                 </div>
@@ -11,20 +11,33 @@
                 </div>
             </div>
         </div>
-        <img v-for="thumbnail, index in thumbnails" :key="index" @click="imgIndex = index" class="thumbnail"
-            :src="thumbnail" alt="">
+        <div v-for="thumbnail, index in thumbnails" :key="index" @click="imgIndex = index" :class="[imgIndex === index ? 'thumbnail active' : 'thumbnail']">
+            <img :src="thumbnail"  alt="thumbnail image">
+            <div class="overlay"></div>
+        </div>
     </div>
+    <Teleport to="body">
+    <Lightbox v-if="lightboxOpen" @closeLightbox="lightboxOpen = false" :images="images" :thumbnails="thumbnails" lightbox="true"/>
+    </Teleport>
+
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import Lightbox from '@/components/Lightbox.vue'
 
 const props = defineProps({
     images: Array,
-    thumbnails: Array
+    thumbnails: Array,
+    lightbox: Boolean
 })
 const imgIndex = ref(0)
-
+const lightboxOpen = ref(false)
+const showLightbox = () => {
+    if(!props.lightbox && window.screen.width >= 768) {
+        lightboxOpen.value = true
+    }
+}
 function next() {
     if (imgIndex.value < 3) {
         imgIndex.value++
@@ -52,13 +65,29 @@ function previous() {
 
 .slider {
     grid-area: 1 / 1 / span 4 / span 4;
-    border-radius: 1rem;
-    overflow: hidden;
     position: relative;
+}
+
+.slider img {
+        border-radius: 1rem;
+    overflow: hidden;
 }
 
 .slider-control {
     display: none;
+    position: absolute;
+    top: 50%;
+    left: 1rem;
+    right: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    transform: translateY(-50%);
+}
+
+.slider-control.lightbox-mode {
+    display: flex;
+    left: -1.25rem;
+    right: -1.25rem;
 }
 
 .control-circle {
@@ -73,10 +102,30 @@ function previous() {
 }
 
 .thumbnail {
+    position: relative;
     border-radius: 0.8rem;
+    overflow: hidden;
     cursor: pointer;
 }
 
+.thumbnail.active,
+.thumbnail:hover {
+    border: 2px solid var(--Orange);
+}
+
+.thumbnail.active .overlay {
+    opacity: 1;
+}
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.6);
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+}
 @media screen and (width < 768px) {
     .product-gallery {
         grid-template-rows: repeat(4, 1fr);
@@ -89,15 +138,6 @@ function previous() {
 
     .slider-control {
         display: flex;
-        position: absolute;
-        top: 50%;
-        left: 1rem;
-        right: 1rem;
-        justify-content: space-between;
-        align-items: center;
-        transform: translateY(-50%);
-
-
     }
 
     .thumbnail {
